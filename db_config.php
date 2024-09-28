bf8f824618607a4557b8ab5f82d73be005aa6b61
nahidibnaharun <96371924+nahidibnaharun@users.noreply.github.com>
2024-09-28 23:04:40 +0600

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,13 +1,11 @@
 <?php
 $servername = "localhost";
-$username = "root"; // Default MySQL username
-$password = "";    // Default MySQL password (if you changed it, use your password)
-$dbname = "productivity_db"; // The name of your database
+$username = "root";
+$password = "";    
+$dbname = "productivity_db"; 
 
-// Create a connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Check the connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
